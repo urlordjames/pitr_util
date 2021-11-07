bf0fe1a1e5b97cd7cf1cc4ff1a71998c1b609438
urlordjames <32751441+urlordjames@users.noreply.github.com>
2021-11-07 15:44:02 -0500

    default.nix -> shell.nix and remove pitr_info

--- default.nix
+++
@@ -1,9 +0,0 @@
-let pkgs = import <nixpkgs> {};
-in pkgs.rustPlatform.buildRustPackage rec {
-	pname = "pitr_util";
-	version = "0.1.0";
-
-	src = ./.;
-
-	cargoSha256 = "18hp0ad4h1r99pxr3k8n2bh6mfmyvp1zgqq2g8jjxj0q6agi5isj";
-}
