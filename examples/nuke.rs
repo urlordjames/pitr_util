4da62f9cdd903fde429013628f3395aa43922dbf
urlordjames <32751441+urlordjames@users.noreply.github.com>
2021-11-09 17:55:28 -0500

    refactor Prop to use PropType

--- examples/nuke.rs
+++ examples/nuke.rs
@@ -1,12 +1,12 @@
 use pitr_util::pitrmap::PitrMap;
-use pitr_util::prop::Prop;
+use pitr_util::prop::{Prop, PropType};
 
 fn main() {
 	let mut map = PitrMap::new();
 
 	for x in 0..50 {
 		for z in 0..50 {
-			let mut explosive = Prop::new(String::from("ultrakill.explosive-barrel"));
+			let mut explosive = Prop::new(PropType::ExplosiveBarrel);
 
 			explosive.Position.x = x as f64;
 			explosive.Position.y = 30f64;
--- src/prop.rs
+++ src/prop.rs
@@ -1,25 +1,47 @@
 use serde::{Serialize, Deserialize};
 use crate::transform::{Vec3, Rotation};
 
+#[derive(Serialize, Deserialize, Debug)]
+pub enum PropType {
+	#[serde(rename="ultrakill.barrel")]
+	Barrel,
+	#[serde(rename="ultrakill.explosive-barrel")]
+	ExplosiveBarrel,
+	#[serde(rename="ultrakill.crate")]
+	Crate,
+	#[serde(rename="ultrakill.barrier")]
+	Barrier,
+	#[serde(rename="ultrakill.maurice")]
+	Maurice,
+	#[serde(rename="ultrakill.melon")]
+	Melon,
+	#[serde(rename="ultrakill.grapple-point")]
+	GrapplePoint,
+}
+
 #[allow(non_snake_case)]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Prop {
-	pub ObjectIdentifier: String,
+	pub ObjectIdentifier: PropType,
 	pub Position: Vec3,
 	pub Rotation: Rotation,
 	pub Scale: Vec3,
 	pub Kinematic: bool,
-	pub Type: u8
+	// always 0
+	Type: u8
 }
 
 impl Prop {
 	#[allow(non_snake_case)]
-	pub fn new(ObjectIdentifier: String) -> Self {
+	pub fn new(ObjectIdentifier: PropType) -> Self {
 		Prop {
+			Scale: match &ObjectIdentifier {
+				PropType::Maurice => Vec3::from(0.15),
+				_ => Vec3::from(1.0)
+			},
 			ObjectIdentifier,
 			Position: Vec3::from(0.0),
 			Rotation: Rotation::zero(),
-			Scale: Vec3::from(1.0),
 			Kinematic: false,
 			Type: 0
 		}
