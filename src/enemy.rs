4f04b598b7e1d464d37fd86a16eb74a63258e5c8
urlordjames <32751441+urlordjames@users.noreply.github.com>
2022-02-16 16:52:03 -0500

    update for latest sandbox changes

--- src/enemy.rs
+++ src/enemy.rs
@@ -21,16 +21,32 @@ pub enum EnemyType {
 	Drone,
 	#[serde(rename="ultrakill.streetcleaner")]
 	Streetcleaner,
+	#[serde(rename="ultrakill.v2")]
+	V2First,
 	#[serde(rename="ultrakill.mindflayer")]
 	Mindflayer,
+	#[serde(rename="ultrakill.v2_2")]
+	V2Second,
 	#[serde(rename="ultrakill.malicious-face")]
 	MaliciousFace,
 	#[serde(rename="ultrakill.cerberus")]
 	Cerberus,
 	#[serde(rename="ultrakill.mass")]
 	HideousMass,
+	#[serde(rename="ultrakill.gabriel")]
+	Gabriel,
 	#[serde(rename="ultrakill.virtue")]
-	Virtue
+	Virtue,
+	#[serde(rename="ultrakill.cancerous-rodent")]
+	CancerousRodent,
+	#[serde(rename="ultrakill.very-cancerous-rodent")]
+	VeryCancerousRodent,
+	#[serde(rename="ultrakill.mandalore")]
+	Mandalore,
+	#[serde(rename="ultrakill.flesh-prison")]
+	FleshPrison,
+	#[serde(rename="ultrakill.minos-prime")]
+	MinosPrime
 }
 
 #[allow(non_snake_case)]
@@ -69,11 +85,19 @@ impl Enemy {
 			EnemyType::Swordsmachine => Vec3::from(1.0),
 			EnemyType::Drone => Vec3::from(2.0),
 			EnemyType::Streetcleaner => Vec3::from(1.0),
+			EnemyType::V2First => Vec3::from(1.0),
 			EnemyType::Mindflayer => Vec3::from(1.0),
+			EnemyType::V2Second => Vec3::from(1.0),
 			EnemyType::MaliciousFace => Vec3::from(1.0),
 			EnemyType::Cerberus => Vec3::from(4.0),
 			EnemyType::HideousMass => Vec3::from(1.0),
-			EnemyType::Virtue => Vec3::from(1.0)
+			EnemyType::Gabriel => Vec3::from(1.0),
+			EnemyType::Virtue => Vec3::from(1.0),
+			EnemyType::CancerousRodent => Vec3::from(0.2),
+			EnemyType::VeryCancerousRodent => Vec3::from(3.0),
+			EnemyType::Mandalore => Vec3::from(1.0),
+			EnemyType::FleshPrison => Vec3::from(4.0),
+			EnemyType::MinosPrime => Vec3::from(1.0)
 		}
 	}
 }
